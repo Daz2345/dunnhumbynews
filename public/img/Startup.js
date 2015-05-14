b6e089b79ec6a38d56e958dccc6c4f0329e59ed1
daz2345 <darrenimpey@btinternet.com>
2015-05-14 14:59:59 +0000

    First commit

---
+++ public/img/Startup.js
@@ -0,0 +1,27 @@
+Meta.set([{
+    name: "name",
+    property: "viewport",
+    content: "width=device-width, initial-scale=1, user-scalable=no, minimal-ui"
+},{
+    name: "name",
+    property: "apple-mobile-web-app-capable",
+    content: "yes"
+}, {
+    name: "name",
+    property: "mobile-web-app-capable",
+    content: "yes"
+}, {
+    name: "name",
+    property: "apple-mobile-web-app-title",
+    content: "dunnhumby"
+}, {
+    name: "name",
+    property: "apple-mobile-web-app-status-bar-style",
+    content: "black"
+}]);
+
+Meta.config({
+  options: {
+    title: "Retail by dunnhumby"
+  }
+});
